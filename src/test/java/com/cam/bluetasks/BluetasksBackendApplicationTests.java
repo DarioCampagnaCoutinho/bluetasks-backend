c588214129f3c548a61ac5f99c120135ef072d96
DarioCampagnaCoutinho <campagnacoutinho67@gmail.com>
2020-09-07 20:39:43 -0300

    Criando o projeto.

---
+++ src/test/java/com/cam/bluetasks/BluetasksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cam.bluetasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluetasksBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
