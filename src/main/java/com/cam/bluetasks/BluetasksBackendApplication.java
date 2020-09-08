288110950e1fb8bee4af816fab265ef531d11b0a
DarioCampagnaCoutinho <campagnacoutinho67@gmail.com>
2020-09-08 08:23:09 -0300

    Expondo o id das tarefas via web services.

--- src/main/java/com/cam/bluetasks/BluetasksBackendApplication.java
+++ src/main/java/com/cam/bluetasks/BluetasksBackendApplication.java
@@ -2,12 +2,21 @@ package com.cam.bluetasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import com.cam.bluetasks.domain.task.Task;
 
 @SpringBootApplication
-public class BluetasksBackendApplication {
+public class BluetasksBackendApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BluetasksBackendApplication.class, args);
+	}
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Task.class);
 	}
 
 }
