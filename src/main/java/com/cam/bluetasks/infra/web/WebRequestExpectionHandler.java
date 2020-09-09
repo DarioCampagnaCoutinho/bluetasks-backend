625808db5c3029253d64567641662d7846f9df34
DarioCampagnaCoutinho <campagnacoutinho67@gmail.com>
2020-09-09 10:07:05 -0300

    Validando tarefas duplicadas.

--- src/main/java/com/cam/bluetasks/infra/web/WebRequestExpectionHandler.java
+++ src/main/java/com/cam/bluetasks/infra/web/WebRequestExpectionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.cam.bluetasks.domain.task.DuplicateTaskException;
+
 @RestControllerAdvice
 public class WebRequestExpectionHandler {
 	
@@ -14,6 +16,11 @@ public class WebRequestExpectionHandler {
 	public RestResponseError handleException(RepositoryConstraintViolationException e) {
 		return RestResponseError.fromValidationError(e.getErrors());
 	}
-
+	
+	@ExceptionHandler
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public RestResponseError handleException(DuplicateTaskException e) {
+		return RestResponseError.fromMessage(e.getMessage());
+	}
 
 }
