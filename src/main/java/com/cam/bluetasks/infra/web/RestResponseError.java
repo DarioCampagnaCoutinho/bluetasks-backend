f4eeadc98e6b3f48d6ab6518b1645a20553a8519
DarioCampagnaCoutinho <campagnacoutinho67@gmail.com>
2020-09-09 09:42:41 -0300

    Mudando a forma de como os erros são retornados.

---
+++ src/main/java/com/cam/bluetasks/infra/web/RestResponseError.java
@@ -0,0 +1,33 @@
+package com.cam.bluetasks.infra.web;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+
+public class RestResponseError {
+	
+	private String error;
+	
+	private RestResponseError() {
+		
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+	
+	public static RestResponseError fromValidationError(Errors errors) {
+		
+		RestResponseError resp = new RestResponseError();
+		StringBuilder sb = new StringBuilder();
+		for(ObjectError error : errors.getAllErrors()) {
+			sb.append(error.getDefaultMessage()).append(". ");
+		}
+		resp.error = sb.toString();
+		return resp;
+	}
+
+}
